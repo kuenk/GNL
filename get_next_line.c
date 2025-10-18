085ad5f291908d86a128addde0e2e456290147cb
David Cuenca <111570446+kuenk@users.noreply.github.com>
2025-10-18 20:40:31 +0200

    Add files via upload

---
+++ get_next_line.c
@@ -0,0 +1,21 @@
+
+
+
+
+#include "get_next_line.h"
+
+char    *get_next_line(int fd)
+{
+    static char 
+    char    *
+
+     = read_buffer(fd, );
+     if( == NULL)
+     {
+        free();
+        = NULL,
+        return(NULL);
+
+     }
+
+}
